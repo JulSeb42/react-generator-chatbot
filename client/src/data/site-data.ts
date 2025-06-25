b3ec36aacbf9f943ce8b34d1ed2a6396984f32f1
Julien <julien.sebag@me.com>
2025-06-25 15:45:43 +0200

    Fix metadata

--- client/src/data/site-data.ts
+++ client/src/data/site-data.ts
@@ -1,6 +1,7 @@
 export const SITE_DATA = {
 	NAME: "React Chatbot",
-	DESCRIPTION: "",
+	DESCRIPTION:
+		"Final project for Ironhack AI Engineering bootcamp, by Julien Sebag",
 	KEYWORDS: [],
 	FAVICON: "/favicon.ico",
 	AUTHOR: "Julien Sebag",
